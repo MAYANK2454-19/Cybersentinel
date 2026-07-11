2c79576ba9d2835d76eb06fc751ecbafac4b8ed0
MAYANK2454-19 <mk2599599@gmail.com>
2026-07-11 13:42:34 +0530

    added new version of whois lookup

--- whois_tool/whois_lookup.py
+++ whois_tool/whois_lookup.py
@@ -26,14 +26,43 @@ def whois_lookup(domain):
         data = whois.whois(domain)
 
         report = {
+
     "domain": clean_value(data.domain_name),
+
     "registrar": clean_value(data.registrar),
+
     "creation_date": format_date(data.creation_date),
+
     "expiration_date": format_date(data.expiration_date),
+
     "updated_date": format_date(data.updated_date),
+
+    "registrant": clean_value(getattr(data, "name", None)) or "N/A",
+
+    "organization": clean_value(getattr(data, "org", None)) or "N/A",
+
+    "email": clean_value(getattr(data, "emails", None)) or "N/A",
+
+    "phone": clean_value(getattr(data, "phone", None)) or "N/A",
+
+    "address": clean_value(getattr(data, "address", None)) or "N/A",
+
+    "city": clean_value(getattr(data, "city", None)) or "N/A",
+
+    "state": clean_value(getattr(data, "state", None)) or "N/A",
+
+    "country": clean_value(getattr(data, "country", None)) or "N/A",
+
+    "zipcode": clean_value(getattr(data, "zipcode", None)) or "N/A",
+
+    "dnssec": clean_value(getattr(data, "dnssec", None)) or "N/A",
+
+    "status": clean_value(getattr(data, "status", None)) or "N/A",
+
     "name_servers": sorted(list(data.name_servers))
     if data.name_servers
     else []
+
 }
 
         return report
